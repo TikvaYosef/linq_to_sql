e09864cb2d94a4572e77d928cafc951a1a4d679b
TikvaYosef <tikvayosef13@gmail.com>
2022-01-02 15:59:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq_to_sql
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string conectionString = "Data Source=desktop-5e70rm2;Initial Catalog=dataDb;Integrated Security=True;Pooling=False";
+            DataClasses1DataContext data = new DataClasses1DataContext(conectionString);
+
+            foreach (var dogitem in data.Dogs)
+            {
+                Console.WriteLine(dogitem.age);
+            }
+            Console.ReadLine();
+          
+        }
+    }
+}
